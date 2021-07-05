1f6c3f7fe87d3c739cc5abd9565b8a33a5632750
= <=>
2021-07-05 16:06:05 +0800

    '去除工具类'

--- src/main/java/com/snail/web/utils/AliSmsCodeUtil.java
+++ src/main/java/com/snail/web/utils/AliSmsCodeUtil.java
@@ -1,14 +1,6 @@
 package com.snail.web.utils;
 
 import com.alibaba.fastjson.JSON;
-import com.aliyuncs.CommonRequest;
-import com.aliyuncs.CommonResponse;
-import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.IAcsClient;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.exceptions.ServerException;
-import com.aliyuncs.http.MethodType;
-import com.aliyuncs.profile.DefaultProfile;
 
 import java.util.Map;
 
@@ -21,11 +13,11 @@ public class AliSmsCodeUtil {
 
 
 	public static String sendVertifySms(String phone ,String msg){
-
+       return null;
 	}
 
     public static String sendSms(String phone,String msg) {
-    	
+		return null;
     }
 
     public static void main(String[] args) {
